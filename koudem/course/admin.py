a1a577fdc18036c5087131c0f10a4433c630d604
Martin Chilpa <martinc1399@gmail.com>
2024-06-09 18:56:27 -0600

    Creacion de estilos

--- koudem/course/admin.py
+++ koudem/course/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Course
 
-# Register your models here.
+class CourseAdmin(admin.ModelAdmin):
+    list_display=("name","category")
+    # search_fields=("name","email")
+    # list_filter = ("name",)
+    # date_hierarchy="birthday"
+
+admin.site.register(Course,CourseAdmin)
+
+
